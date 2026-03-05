f23db3647a7fca955111b743798c3cf16f5a5ea1
pringle-1 <23178@burnside.school.nz>
2026-03-06 12:45:10 +1300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# Import models
+from flask import Flask, g, render_template, request
+import sqlite3
+# Create app instance
+app = Flask(__name__)
+# Route for index (home) page
+@app.route('/')
+# Define function that runs when route is served
+def home ():
+    return "On Repeat"
+# Run statement
+if __name__ == "__main__":
+    app.run(debug=True)
