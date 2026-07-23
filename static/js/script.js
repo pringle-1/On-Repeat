cff58ac4cf26daca44e9e46d415b2de97f5d1118
pringle-1 <23178@burnside.school.nz>
2026-07-23 20:24:06 +1200

    made it so that the option to view password only shows when there is text in the password field

--- static/js/script.js
+++ static/js/script.js
@@ -15,5 +15,17 @@ function hidePassword() {
     }
 }
 
+function toggleEye() {
+    const password = document.getElementById("password");
+    const eye = document.getElementById("password-button");
+
+    if (password.value.length > 0) {
+        eye.style.display = "block";
+    } else {
+        eye.style.display = "none";
+        hidePassword();
+    }
+}
+
 document.addEventListener("mouseup", hidePassword);
 document.addEventListener("touchend", hidePassword);
