144a32fd0b5c334ded5d6000a03af933b4ad8a8c
pringle-1 <23178@burnside.school.nz>
2026-08-20 00:36:16 +1200

    started working on replies feature by addying the reply table in the database and added the html

--- app.py
+++ app.py
@@ -213,8 +213,15 @@ def review_page(id):
     # Only one review from its ID
     sql = """SELECT Review.*, User.username, User.profile_picture, Album.album_title, Album.album_cover, Artist.artist_id, Artist.artist_name FROM review JOIN User ON Review.user_id = User.user_id JOIN Album ON Review.album_id = Album.album_id JOIN Artist ON Album.artist_id = Artist.artist_id WHERE review_id = ?;"""
     commentsql = """SELECT Comment.*, User.username, User.profile_picture FROM Comment JOIN User ON Comment.user_id = User.user_id WHERE review_id = ? ORDER BY comment_id DESC;"""
+    replysql = """SELECT Reply.*, User.username, User.profile_picture FROM Reply JOIN User ON Reply.user_id = User.user_id WHERE comment_id = ? ORDER BY reply_id ASC;"""
     review = query_db(sql,(id,), True)
     comments = query_db(commentsql, (id,))
+    comment_list = []
+    for comment in comments:
+        comment_data = dict(comment)
+        comment_data['replies'] = query_db(replysql, (comment['comment_id'],))
+        comment_list.append(comment_data)
+    comments = comment_list
     if review is None:
         abort(404)
     if request.method == 'POST':
@@ -226,7 +233,6 @@ def review_page(id):
         db.commit()
         return redirect(url_for('review_page', id=id))
     return render_template("review.html", review=review, comments=comments)
-
 
 # Route for editing a review
 @app.route('/review/<int:id>/edit', methods=['GET', 'POST'])
